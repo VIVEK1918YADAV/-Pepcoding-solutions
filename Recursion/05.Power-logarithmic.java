2f4fb09bd249d0272a46361f24fa6090e6da69f9
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2022-05-01 08:57:23 +0530

    Create 05.Power-logarithmic.java

---
+++ Recursion/05.Power-logarithmic.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+
+        int ans = power(x,n);
+        System.out.println(ans);
+    }
+
+    public static int power(int x, int n){
+        if(n==0) {
+            return 1;
+        }
+
+        int pxnby2 = power(x,n/2);
+
+        if(n%2==1) {
+            return  pxnby2 * pxnby2 * x;
+            
+        }
+        else 
+        return pxnby2 * pxnby2;
+    }
+
+}
