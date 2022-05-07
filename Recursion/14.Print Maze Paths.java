6108aa536dda80d262cfbadebf8e423f795faf3b
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2022-05-07 06:46:35 +0530

    Create 14.Print Maze Paths.java

---
+++ Recursion/14.Print Maze Paths.java
@@ -0,0 +1,38 @@
+import java.io.*;
+	import java.util.*;
+
+	public class Main {
+
+	    public static void main(String[] args) throws Exception {
+	        Scanner scn = new Scanner(System.in);
+	        int n = scn.nextInt();
+	         int m = scn.nextInt();
+	         
+	         printMazePaths(0, 0, n-1, m-1, "");
+
+
+	    }
+
+	    // sr - source row
+	    // sc - source column
+	    // dr - destination row
+	    // dc - destination column
+	    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+	        
+	      //negative base case
+	      if(sr > dr || sc>dc) {
+	          return ;
+	      }  
+	      //positive base case
+	        if(sr == dr && sc == dc) {
+	            System.out.println(psf);
+	            return;
+	        }
+	        
+	        // Recursive call
+	        printMazePaths(sr, sc+1, dr, dc,psf +"h"); //horizontal
+	        printMazePaths(sr+1, sc, dr, dc,psf +"v"); //vertical	        
+	        
+	    }
+
+	}
