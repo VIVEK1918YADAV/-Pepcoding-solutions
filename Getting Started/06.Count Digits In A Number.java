99c2d37d4f72cff2b6c8160d2346205571d903bb
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2022-11-17 16:18:24 +0530

    Create 06.Count Digits In A Number.java

---
+++ Getting Started/06.Count Digits In A Number.java
@@ -0,0 +1,17 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+    // write your code here 
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    
+    int dig = 0;
+    while(n != 0) {
+        n = n/10;
+        dig++;
+    }
+    System.out.println(dig);
+}
+  }
