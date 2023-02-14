0f4e0fb00bc3fbafd8047237272a4cb7594e77f5
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2023-02-15 03:34:51 +0530

    Update BarChart.java

--- BarChart.java
+++ BarChart.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Barchart {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		 Scanner s=new Scanner(System.in);
 		    int n=s.nextInt();
 		    int arr[]=new int[n];
