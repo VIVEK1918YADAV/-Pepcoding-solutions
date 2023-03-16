7fd2b5535dde80a0a491d7a3831402a8ab8abf64
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2023-03-17 03:46:57 +0530

    Create 09.Target Sum Pair In Bst.java

---
+++ Binary Search Tree/09.Target Sum Pair In Bst.java
@@ -0,0 +1,154 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
+        }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
+    }
+
+    return root;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+  public static void inorder(Node root, ArrayList<Integer> arr){
+      if(root == null) return ;
+      inorder(root.left, arr);
+      arr.add(root.data);
+      inorder(root.right,arr);
+  }
+  public static void approach2(Node root, int target){
+      ArrayList<Integer> arr = new ArrayList<>();//o(n)
+      inorder(root, arr);//insert element in sorted order in arraylist - o(n)
+      int l = 0, r = arr.size()-1;
+      while(l < r){
+          int sum = arr.get(l) + arr.get(r);
+          if(sum == target){
+              System.out.println(arr.get(l) + " "+ arr.get(r));
+              l++;
+              r--;
+          }
+          else if(sum < target){
+              l++;
+          }
+          else{
+              r--;
+          }
+      }
+  }
+  
+  //Time - o(n*n)  space - o(h)=> constant(recursion space)
+    public static boolean find(Node curr, int target){
+    if(curr == null) return false;
+    if(curr.data == target) return true;
+    
+    if(curr.data < target) return find(curr.right, target);
+    return find(curr.left, target);
+  }
+  public static void approach1Helper(Node curr, int target, Node root){
+      if(curr == null) return;
+      
+      approach1Helper(curr.left, target, root);
+      
+      if(curr.data >= target/2) return;
+      
+      int complement = target - curr.data;
+      if(find(root, complement) == true){
+          System.out.println(curr.data + " " + complement);
+      }
+      
+      approach1Helper(curr.right, target, root);
+  }
+  public static void approach1(Node root, int target){
+     approach1Helper(root, target, root);
+  }
+  
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    int data = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    // write your code here
+    //approach2(root, data);
+    approach1Helper(root, data, root);
+  }
+
+}
